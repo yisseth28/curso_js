62900c1525a65cb66aaf7f0becd227dce8bae744
andrea zarate <yisseth28@hotmail.com>
2024-11-09 12:13:54 -0500

    ACTIVIDAD IF - ELSE

---
+++ ACTIVIDAD IF - ELSE/index.js
@@ -0,0 +1,48 @@
+//Comparar dos números y determinar si son iguales
+n1=prompt("Ingresa el primer número")
+n2=prompt("Ingresa el segundo número")
+
+if(n1===n2){
+    alert(`El numero ${n1} es igual a ${n2}`)
+}else{
+    alert(`El número ${n1} es diferente a ${n2}`)
+}
+    
+
+//Identificar cual de dos números es el mayor
+n1=prompt("Ingresa el primer número")
+n2=prompt("Ingresa el segundo número")
+
+if(n1==n2){
+    alert(`los números ${n1} y ${n2} son iguales`)
+}else if (n1>n2){
+    alert(`El número ${n1} es mayor a ${n2}`)
+}else{
+    alert(`El número ${n1} es menor a ${n2}`)
+}
+    
+
+//Decide si un número es par o impar
+n1=prompt("Ingresar el número a analizar")
+if(n1 % 2 == 0 ){
+    alert(`El número ingresado ${n1}, es par`)
+}else{
+    alert(`El número ingresado ${n1}, es impar`)
+}
+
+//Clasificar los triangulos según sus lados
+l1=prompt("Ingresar el lado1 del triangulo")
+l2=prompt("Ingresar el lado2 del triangulo")
+l3=prompt("Ingresar el lado3 del triangulo")
+
+if(l1 == l2 ){
+}else if ((l2 == l3)){
+    alert(`El triangulo es Equilatero ya que todos sus lados son iguales: ${l1}, ${l2}, ${l3}`)
+}else if (l2!=l3){
+
+}else if(l){
+    alert(`El triangulo es Isosceles ya que sus lados son 2 lados iguales: ${l1}, ${l2}, ${l3}`)
+}    
+    
+
+    
